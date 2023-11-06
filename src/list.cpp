412875d188f38aeee3159fe137c11e81ee2eec34
eg-mart <marteg71@gmail.com>
2023-11-06 19:04:38 +0300

    implement list insert function

---
+++ src/list.cpp
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+
+#include "list.h"
+
+int list_ctor(struct List *list)
+{
+	list->capacity = INIT_CAPACITY;
+	list->prev = (int*) calloc(INIT_CAPACITY, sizeof(int));
+	if (!list->prev) return 1;
+	list->next = (int*) calloc(INIT_CAPACITY, sizeof(int));
+	if (!list->next) return 1;
+	list->data = (int*) calloc(INIT_CAPACITY, sizeof(int));
+	if (!list->data) return 1;
+
+	for (size_t i = 0; i < INIT_CAPACITY; i++) {
+		list->data[i] = POISON;
+		list->prev[i] = -1;
+	}
+
+	list->size = 1;
+	list->head = 0;
+	list->tail = 0;
+	list->next[0] = 0;
+	list->prev[0] = 0;
+	list->free = 1;
+	for (size_t i = 1; i < INIT_CAPACITY; i++)
+		list->next[i] = (int) i + 1;
+	list->next[INIT_CAPACITY - 1] = 0;
+
+	return 0;
+}
+
+void list_dtor(struct List *list)
+{
+	free(list->data);
+	list->data = NULL;
+	free(list->prev);
+	list->prev = NULL;
+	free(list->next);
+	list->next = NULL;
+	list->capacity = 0;
+	list->head = 0;
+	list->tail = 0;
+	list->free = 0;
+	list->size = 0;
+}
+
+int list_insert(struct List *list, size_t dest, int x, size_t *ind)
+{
+	if (list->capacity == list->size)
+		return 1;
+
+	if (list->free == 0) //this shouldn't happen if there is free space
+		return 1;
+
+	*ind = (size_t) list->free;
+	list->free = list->next[list->free];
+
+	list->data[*ind] = x;
+	list->next[*ind] = (int) dest;
+	list->prev[*ind] = list->prev[dest];
+	list->next[list->prev[dest]] = (int) *ind;
+	list->prev[dest] = (int) *ind;
+
+	list->head = list->next[0];
+	list->tail = list->prev[0];
+
+	return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+#include "list_debug.h"
+#include "list.h"
+
 int main()
 {
-	printf("Hello world!\n");
+	struct List list = {};
+	list_ctor(&list);
+
+	size_t ind = 0;
+	for (int i = 0; i < 10; i++)
+		list_insert(&list, list.head, i, &ind);
+	list_insert(&list, 0, 20, &ind);
+	list_graphic_dump(&list);
+	list_dtor(&list);
 	return 0;
 }
